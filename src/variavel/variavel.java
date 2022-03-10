c5f8be6b5b1b2c252a08d6121f7918cee601c184
eniohcs <enio.custoias@gmail.com>
2022-03-10 17:57:22 -0300

    treino de variaveis

---
+++ src/variavel/variavel.java
@@ -0,0 +1,39 @@
+package variavel;
+
+
+
+public class variavel {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int I = 20;
+        int _1a = 35;
+        int $aq = 15;
+
+        final int j = 10;
+        int asrn24678md;
+        int asrn2$4678_md;
+
+        asrn24678md = 100;
+        asrn2$4678_md = 10;
+
+        int quantidadeProduto = 50;
+        final int NUMERO_TENTATIVAS = 5;
+        int QUANTIDADE_OPCOES = 25;
+
+        System.out.println("i =  " + i);
+        System.out.println("I = " + I);
+        System.out.println("_1a = " + _1a);
+        System.out.println("$aq = " + $aq);
+
+        System.out.println("j = " + j);
+        System.out.println("asrn24678md = " + asrn24678md);
+        System.out.println("asrn2$4678_md = " + asrn2$4678_md);
+
+        System.out.println("quantidadeProduto = " + quantidadeProduto);
+        System.out.println("NUMERO_TENTATIVAS = " + NUMERO_TENTATIVAS);
+        System.out.println("QUANTIDADE_OPCOES = " + QUANTIDADE_OPCOES);
+
+
+    }
+}
